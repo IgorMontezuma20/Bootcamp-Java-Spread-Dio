36d66ddda382897f19245c41d335e8cbe9794dfc
Igor Montezuma <igorminranda17@gmail.com>
2021-12-09 14:07:41 -0300

    primeiro commit

---
+++ src/com/dio/PrimeiroProagrama.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public class PrimeiroProagrama {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println("Olá Mundo!");
+		System.out.println("A soma entre os valores "+a+" e "+b+" é de "+(a+b));
+
+	}
+
+}
